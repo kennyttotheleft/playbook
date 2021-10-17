54315880ac3eab324d356a1251b9756cdbc2af24
tanakaoriginal <tanakaoriginal@users.noreply.github.com>
2021-10-17 09:08:14 +0000

    deploy: 251bfa46fdaff4c59dda7811fc688d915d558e2e

--- __tests__/unit/e2e/helper/SiteHelper.test.js
+++ __tests__/unit/e2e/helper/SiteHelper.test.js
@@ -18,51 +18,76 @@ describe('SiteHelper', () => {
     process.env = OLD_ENV; // Restore old environment
   });
 
-  testTitle = 'lang: %s, getPagePath(%s)';
+  testTitle = 'env: %s, lang: %s, getPagePath(%s) -> %s';
   test.each([
-    ['en_us', '', '/'],
-    ['en_us', '/', '/'],
-    ['en_us', 'cookie/', '/cookie/'],
-    ['ja_jp', '', '/ja_jp/'],
-    ['ja_jp', '/', '/ja_jp/'],
-    ['ja_jp', 'cookie/', '/ja_jp/cookie/']
-  ])(testTitle, (lang, path, expected) => {
+    ['', 'en_us', '', ''],
+    ['', 'en_us', '/', '/'],
+    ['', 'en_us', '/cookie', '/cookie/'],
+    ['develpment', 'en_us', '', ''],
+    ['develpment', 'en_us', '/', '/'],
+    ['develpment', 'en_us', '/cookie', '/cookie/'],
+    ['staging', 'en_us', '', ''],
+    ['staging', 'en_us', '/', '/'],
+    ['staging', 'en_us', '/cookie', '/cookie/'],
+    ['testing', 'en_us', '', ''],
+    ['testing', 'en_us', '/', '/'],
+    ['testing', 'en_us', '/cookie', '/cookie/'],
+    ['production', 'en_us', '', ''],
+    ['production', 'en_us', '/', '/playbook/'],
+    ['production', 'en_us', '/cookie', '/playbook/cookie/'],
+    ['', 'ja_jp', '', ''],
+    ['', 'ja_jp', '/', '/ja_jp/'],
+    ['', 'ja_jp', '/cookie', '/ja_jp/cookie/'],
+    ['develpment', 'ja_jp', '', ''],
+    ['develpment', 'ja_jp', '/', '/ja_jp/'],
+    ['develpment', 'ja_jp', '/cookie', '/ja_jp/cookie/'],
+    ['staging', 'ja_jp', '', ''],
+    ['staging', 'ja_jp', '/', '/ja_jp/'],
+    ['staging', 'ja_jp', '/cookie', '/ja_jp/cookie/'],
+    ['testing', 'ja_jp', '', ''],
+    ['testing', 'ja_jp', '/', '/ja_jp/'],
+    ['testing', 'ja_jp', '/cookie', '/ja_jp/cookie/'],
+    ['production', 'ja_jp', '', ''],
+    ['production', 'ja_jp', '/', '/playbook/ja_jp/'],
+    ['production', 'ja_jp', '/cookie', '/playbook/ja_jp/cookie/'],
+  ])(testTitle, (env, lang, path, expected) => {
+    process.env.ENVIRONMENT = env;
     const siteHelper = new SiteHelper(lang);
     expect(siteHelper.getPagePath(path)).toBe(expected);
   });
 
-  testTitle = 'env: %s, lang: %s, getPageUrl(%s)';
+  testTitle = 'env: %s, lang: %s, getPageUrl(%s) -> %s';
   test.each([
-    ['', 'en_us', '', 'http://localhost:4000/'],
+    ['', 'en_us', '', ''],
     ['', 'en_us', '/', 'http://localhost:4000/'],
-    ['', 'en_us', 'cookie/', 'http://localhost:4000/cookie/'],
-    ['', 'ja_jp', '', 'http://localhost:4000/ja_jp/'],
+    ['', 'en_us', '/cookie', 'http://localhost:4000/cookie/'],
+    ['', 'ja_jp', '', ''],
     ['', 'ja_jp', '/', 'http://localhost:4000/ja_jp/'],
-    ['', 'ja_jp', 'cookie/', 'http://localhost:4000/ja_jp/cookie/'],
-    ['develpment', 'en_us', '', 'http://localhost:4000/'],
+    ['', 'ja_jp', '/cookie', 'http://localhost:4000/ja_jp/cookie/'],
+    ['develpment', 'en_us', '', ''],
     ['develpment', 'en_us', '/', 'http://localhost:4000/'],
-    ['develpment', 'en_us', 'cookie/', 'http://localhost:4000/cookie/'],
-    ['develpment', 'ja_jp', '', 'http://localhost:4000/ja_jp/'],
+    ['develpment', 'en_us', '/cookie', 'http://localhost:4000/cookie/'],
+    ['develpment', 'ja_jp', '', ''],
     ['develpment', 'ja_jp', '/', 'http://localhost:4000/ja_jp/'],
-    ['develpment', 'ja_jp', 'cookie/', 'http://localhost:4000/ja_jp/cookie/'],
-    ['staging', 'en_us', '', '/'],
+    ['develpment', 'ja_jp', '/cookie', 'http://localhost:4000/ja_jp/cookie/'],
+    ['staging', 'en_us', '', ''],
     ['staging', 'en_us', '/', '/'],
-    ['staging', 'en_us', 'cookie/', '/cookie/'],
-    ['staging', 'ja_jp', '', '/ja_jp/'],
+    ['staging', 'en_us', '/cookie', '/cookie/'],
+    ['staging', 'ja_jp', '', ''],
     ['staging', 'ja_jp', '/', '/ja_jp/'],
-    ['staging', 'ja_jp', 'cookie/', '/ja_jp/cookie/'],
-    ['testing', 'en_us', '', '/'],
+    ['staging', 'ja_jp', '/cookie', '/ja_jp/cookie/'],
+    ['testing', 'en_us', '', ''],
     ['testing', 'en_us', '/', '/'],
-    ['testing', 'en_us', 'cookie/', '/cookie/'],
-    ['testing', 'ja_jp', '', '/ja_jp/'],
+    ['testing', 'en_us', '/cookie', '/cookie/'],
+    ['testing', 'ja_jp', '', ''],
     ['testing', 'ja_jp', '/', '/ja_jp/'],
-    ['testing', 'ja_jp', 'cookie/', '/ja_jp/cookie/'],
-    ['production', 'en_us', '', 'https://tanakaoriginal.github.io/playbook/'],
+    ['testing', 'ja_jp', '/cookie', '/ja_jp/cookie/'],
+    ['production', 'en_us', '', ''],
     ['production', 'en_us', '/', 'https://tanakaoriginal.github.io/playbook/'],
-    ['production', 'en_us', 'cookie/', 'https://tanakaoriginal.github.io/playbook/cookie/'],
-    ['production', 'ja_jp', '', 'https://tanakaoriginal.github.io/playbook/ja_jp/'],
+    ['production', 'en_us', '/cookie', 'https://tanakaoriginal.github.io/playbook/cookie/'],
+    ['production', 'ja_jp', '', ''],
     ['production', 'ja_jp', '/', 'https://tanakaoriginal.github.io/playbook/ja_jp/'],
-    ['production', 'ja_jp', 'cookie/', 'https://tanakaoriginal.github.io/playbook/ja_jp/cookie/'],
+    ['production', 'ja_jp', '/cookie', 'https://tanakaoriginal.github.io/playbook/ja_jp/cookie/'],
   ])(testTitle, (env, lang, path, expected) => {
     process.env.ENVIRONMENT = env;
     const siteHelper = new SiteHelper(lang);
